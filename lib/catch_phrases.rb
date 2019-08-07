f17bf0894b9602cd3402fb6ea2386979b0de3369
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-07 04:33:40 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,5 +13,7 @@ def link
   puts "It's Dangerous To Go Alone! Take This."
 end
 
-def any_phrase
-  
+def any_phrase(phrase ="Do A Barrel Roll!")
+  phrase = "Do A Barrel Roll!"
+  puts phrase
+end
